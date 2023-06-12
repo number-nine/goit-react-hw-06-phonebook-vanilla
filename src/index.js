a3f0ec1415264afedc006a94653c72aeea0ed26d
number-nine <sergii.pokhylko@gmail.com>
2023-06-12 22:35:21 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-06-phonebook-toolkit">
+    <BrowserRouter basename="/goit-react-hw-06-phonebook-vanilla">
       <Provider store={store}>
         <App />
       </Provider>
